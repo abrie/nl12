69e92508680034a5bb9c862c5027a5b361a5118e
abrie <abrie@users.noreply.github.com>
2024-11-06 11:05:21 -0500

    Add TilemapManager class to encapsulate a configurable tilemap
    * Define `TilemapManager` class with properties for scene, tilemap, emptyTileset, filledTileset, and layer
    * Generate empty and filled textures using `TextureGenerator.generateTexture`
    * Create separate tilesetImages for empty and filled textures
    * Set up collision detection for filled spaces using `firstgid` property
    * Update `setTile` method to use `firstgid` for tile index

--- src/utils/TilemapManager.ts
+++ src/utils/TilemapManager.ts
@@ -4,7 +4,8 @@ import TextureGenerator from "./TextureGenerator";
 class TilemapManager {
   private scene: Phaser.Scene;
   private tilemap: Phaser.Tilemaps.Tilemap;
-  private tileset: Phaser.Tilemaps.Tileset;
+  private emptyTileset: Phaser.Tilemaps.Tileset;
+  private filledTileset: Phaser.Tilemaps.Tileset;
   private layer: Phaser.Tilemaps.TilemapLayer;
 
   constructor(scene: Phaser.Scene) {
@@ -17,18 +18,19 @@ class TilemapManager {
     const filledTexture = TextureGenerator.generateTexture(this.scene, 0x000000, 32, 32, "filled");
 
     this.tilemap = this.scene.make.tilemap({ width: 10, height: 10, tileWidth: 32, tileHeight: 32 });
-    this.tileset = this.tilemap.addTilesetImage("tiles", null, 32, 32, 0, 0, [emptyTexture, filledTexture]);
-    this.layer = this.tilemap.createBlankLayer("layer", this.tileset);
+    this.emptyTileset = this.tilemap.addTilesetImage("empty", emptyTexture);
+    this.filledTileset = this.tilemap.addTilesetImage("filled", filledTexture);
+    this.layer = this.tilemap.createBlankLayer("layer", [this.emptyTileset, this.filledTileset]);
 
     this.setupCollision();
   }
 
   private setupCollision() {
-    this.layer.setCollisionBetween(1, 1);
+    this.layer.setCollisionBetween(this.filledTileset.firstgid, this.filledTileset.firstgid);
   }
 
   public setTile(x: number, y: number, filled: boolean) {
-    const tileIndex = filled ? 1 : 0;
+    const tileIndex = filled ? this.filledTileset.firstgid : this.emptyTileset.firstgid;
     this.tilemap.putTileAt(tileIndex, x, y, true, this.layer);
   }
 }
