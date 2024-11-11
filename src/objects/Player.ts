1df2e62c758d1d6ed2a20024f013eb5fe5aa0acd
abrie <abrie@hello>
2024-11-10 21:17:12 -0500

    Remove world bounds collider

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -34,14 +34,6 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 		this.setTexture("player");
 		this.setOrigin(0, 0);
 		this.body?.setSize(width, height);
-
-		this.scene.physics.add.collider(
-			this,
-			this.scene.physics.world.bounds,
-			this.handleCollision,
-			undefined,
-			this,
-		);
 	}
 
 	private stateMachine = {
